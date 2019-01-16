10c13f212d3c1587e6617d653dfc55c03dde392c
Antoine Barriere <antoine.barriere@fidesio.com>
2019-01-16 20:40:28 +0700

    beginning authentification (prototype without database)

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -2,18 +2,33 @@ import { Injectable } from '@angular/core';
 import { Observable, of, throwError } from 'rxjs';
 import { Credentials, User } from '../models/user';
 
+
+
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   constructor() { }
-
+  users = [
+    {username: 'antoine', password: 'test'},
+    {username: 'truong', password: 'test2'},
+    {username: 'dat', password: 'test3'}
+  ];
   login({ username, password }: Credentials): Observable<User> {
+    let authenf = false;
     /**
      * Simulate a failed login to display the error
      * message for the login form.
      */
-    if (username !== 'truong') {
+    for (const user of this.users) {
+      if (username === user.username && password === user.password) {
+        authenf = true;
+      }
+    }
+    /*if (username !== 'truong') {
+      return throwError('Invalid username or password');
+    }*/
+    if (authenf === false) {
       return throwError('Invalid username or password');
     }
     return of({ name: 'User' });
