e3152d0c8fab92c63d6f3538a6b97a89835e0947
i069734 <dat.banh@sap.com>
2019-01-17 09:49:36 +0700

    Add some modules in package.json

--- models/Backlog.js
+++ models/Backlog.js
@@ -1,11 +1,20 @@
 var mongoose = require('mongoose');
+var autoIncrement = require('mongoose-auto-increment');
 
 var BacklogSchema = new mongoose.Schema({
     id: Number, 
     title: String,
     desc: String,
+    priority: {
+        type: String,
+        enum: ['very_high', 'high', 'medium',"low"]
+    },    
     due_date: { type: Date },
     created_date: { type: Date, default: Date.now },
+    status: {
+        type: String,
+        enum: ['in_progress', 'in_validation', 'done',"delayed"]
+    },  
     column_id: {type: Number, ref: 'Column'}
 });
 BacklogSchema.plugin(autoIncrement.plugin, {model: 'Backlog', field: 'id'});
